f07b343ac6c16441b2ab7d72abf5783638233cb3
Jesus <icedojesus2@gmail.com>
2025-11-13 13:22:59 -0700

    Se implementan metodos eliminarElemento y eliminarInicio ...

--- src/Main.java
+++ src/Main.java
@@ -9,44 +9,44 @@ public class Main {
         o devolver un String para después imprimirlo desde aquí o mostrarlo en un JOptionPane aquí en Main.java).
          */
 
-        /*
-        Pueden usar JOptionPane o hacer la interfaz por consola.
-        Deben definir un menú con las siguientes opciones:
-                "1. Insertar un elemento al inicio\n"+
-                "2. Insertar un elemento al final\n"+
-                "3. Insertar un elemento en orden\n"+
-                "4. Eliminar un elemento al inicio\n"+
-                "5. Eliminar un elemento al final\n"+
-                "6. Eliminar un elemento\n"+
-                "7. Buscar un elemento\n"+
-                "8. Mostrar los datos de inicio a fin\n"+
-                "9. Mostrar los datos de fin a inicio\n"+
-                "10. Salir\n","Menú de opciones",3));
-        */
-        public static void main(String[] args) {
+    /*
+    Pueden usar JOptionPane o hacer la interfaz por consola.
+    Deben definir un menú con las siguientes opciones:
+            "1. Insertar un elemento al inicio\n"+
+            "2. Insertar un elemento al final\n"+
+            "3. Insertar un elemento en orden\n"+
+            "4. Eliminar un elemento al inicio\n"+
+            "5. Eliminar un elemento al final\n"+
+            "6. Eliminar un elemento\n"+
+            "7. Buscar un elemento\n"+
+            "8. Mostrar los datos de inicio a fin\n"+
+            "9. Mostrar los datos de fin a inicio\n"+
+            "10. Salir\n","Menú de opciones",3));
+    */
+    public static void main(String[] args) {
 
-            int opcion = 0;
-            int elemento;
-            ListaDoble lista = new ListaDoble();
+        int opcion = 0;
+        int elemento;
+        ListaDoble lista = new ListaDoble();
 
-            do {
-                try {
-                    opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
-                                    "1. Insertar un elemento al inicio\n" + //TODO @ Víctor Miguel Ochoa Camargo
-                                "2. Insertar un elemento al final\n" + //TODO @ Mia Renee Valenzuela Yescaz
-                                "3. Insertar un elemento en orden\n" + //TODO @ Víctor Miguel Ochoa Camargo
-                                "4. Eliminar un elemento al inicio\n" + //TODO @ Jesus Francisco Villa Icedo
-                                "5. Eliminar un elemento al final\n" + //TODO @ Mia Renee Valenzuela Yescaz
-                                "6. Eliminar un elemento\n" + //TODO @ Jesus Francisco Villa Icedo
-                                "7. Buscar un elemento\n" + //TODO @ Gloria Jesús Sanchez Lopez
-                                "8. Mostrar los datos de inicio a fin\n" + //TODO @
-                                "9. Mostrar los datos de fin a inicio\n" + //TODO @ Gloria Jesús Sanchez Lopez
+        do {
+            try {
+                opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
+                        "1. Insertar un elemento al inicio\n"+ //TODO @ Víctor Miguel Ochoa Camargo
+                                "2. Insertar un elemento al final\n"+ // @ Mia Renee Valenzuela Yescas
+                                "3. Insertar un elemento en orden\n"+ //TODO @ Víctor Miguel Ochoa Camargo
+                                "4. Eliminar un elemento al inicio\n"+ //TODO @ Jesus Francisco Villa Icedo
+                                "5. Eliminar un elemento al final\n"+ // @ Mia Renee Valenzuela Yescas
+                                "6. Eliminar un elemento\n"+ //TODO @ Jesus Francisco Villa Icedo
+                                "7. Buscar un elemento\n"+ //TODO @ Gloria Jesús Sanchez Lopez
+                                "8. Mostrar los datos de inicio a fin\n"+ //TODO @
+                                "9. Mostrar los datos de fin a inicio\n"+ //TODO @ Gloria Jesús Sanchez Lopez
                                 "10. Salir\n",
                         "Menú de opciones", 3));
                 switch (opcion) {
                     case 1://Insertar un elemento al inicio TODO @ Víctor Miguel Ochoa Camargo
                         break;
-                    case 2://Insertar un elemento al final TODO @ Mia Renee Valenzuela Yescaz
+                    case 2://Insertar un elemento al final  @ Mia Renee Valenzuela Yescas
                         break;
                     case 3: //Insertar en orden TODO @ Víctor Miguel Ochoa Camargo
                         break;
@@ -65,9 +65,10 @@ public class Main {
                                     JOptionPane.WARNING_MESSAGE);
                         }
                         break;
-                    case 5: //Eliminar al final TODO @ Mia Renee Valenzuela Yescaz
+                    case 5: //Eliminar al final  @ Mia Renee Valenzuela Yescas
                         break;
-                    case 6: //Eliminar TODO @ Jesus Francisco Villa Icedo
+
+                    case 6: //Eliminar @ Jesus Francisco Villa Icedo
                         try {
                             int elementoBuscado = Integer.parseInt(JOptionPane.showInputDialog(null,
                                     "Ingresa el elemento que a eliminar:",
